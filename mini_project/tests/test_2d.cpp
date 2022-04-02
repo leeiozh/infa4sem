7cc4a0dc900f904290c7143890b9243683d31927
ezhova.ea <ezhova.ea@phystech.edu>
2022-04-02 11:45:46 +0300

    add sinus

--- mini_project/tests/test_2d.cpp
+++ mini_project/tests/test_2d.cpp
@@ -8,8 +8,9 @@
 #include "mini-project/Writer.hpp"
 #include "mini-project/search_methods/Bisection.hpp"
 #include "mini-project/func_refraction/LinearFunction.hpp"
+#include "mini-project/func_refraction/SinusFunction.hpp"
 
-TEST(SIMPLE_2D, SIZE_20) {
+TEST(LIN_2D, SIZE_20) {
 
     const int DIM = 2; // размерность пространства
     int size = 20; // размер квадратной сетки
@@ -29,7 +30,7 @@ TEST(SIMPLE_2D, SIZE_20) {
     ASSERT_NEAR(res, 48.0981, TOLERANCE);
 }
 
-TEST(SIMPLE_2D, SIZE_200) {
+TEST(LIN_2D, SIZE_200) {
 
     const int DIM = 2; // размерность пространства
     int size = 200; // размер квадратной сетки
@@ -49,7 +50,7 @@ TEST(SIMPLE_2D, SIZE_200) {
     ASSERT_NEAR(res, 57.81005859375, TOLERANCE);
 }
 
-TEST(SIMPLE_2D, SIZE_2000) {
+TEST(LIN_2D, SIZE_2000) {
 
     const int DIM = 2; // размерность пространства
     int size = 2000; // размер квадратной сетки
@@ -67,4 +68,25 @@ TEST(SIMPLE_2D, SIZE_2000) {
 
     double res = bisection.search(0, 90, h * (size - 1), h); // запуск метода
     ASSERT_NEAR(res, 66.64306640625, TOLERANCE);
+}
+
+TEST(SIN_2D, SIZE_20) {
+
+    const int DIM = 2; // размерность пространства
+    int size = 20; // размер квадратной сетки
+    double h = 1.; // шаг
+
+    auto n_coeff = std::make_shared<SinusFunction<DIM>>(SinusFunction<DIM>(size * h, size * h, size * h, 1, 3));
+
+    auto mesh = std::make_shared<Mesh<DIM>>(
+            Mesh<DIM>(size, h, n_coeff)); // сетка с заданным шагом и распределением коэффициента преломления на ней
+
+    auto writer = std::make_shared<Writer<DIM>>(Writer<DIM>(std::string(
+            "/home/leeiozh/miptbot/proga/infa4sem/mini_project/out/res_sin_"))); // писатель и путь, куда ему писать
+
+    Bisection<DIM> bisection(n_coeff, mesh, writer); // метод, который будет все считать и заодно отрисовывать
+
+    double res = bisection.search(0, 90, h * (size - 1), h); // запуск метода
+
+    ASSERT_NEAR(res, 41.2427, TOLERANCE);
 }
